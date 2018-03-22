14c338576be68cd60cfc631c1ba5b514d0e78997
howdy39 <tatsuya.howdy@gmail.com>
2018-03-22 22:33:31 +0900

    Success commit

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import { SlackService } from './slack.service';
 declare var global: any;
 
 global.createNewFile = (): void => {
-  const ss = SheetService.hogehoge('New file');
+  const ss = SheetService.createInitialFile('New file');
   ss.getRange('A2').setValue('Happy gas!!!');
+  ss.getRange('A3').setValue('howdy39');
 
   const slackService = new SlackService();
   slackService.postSheetCreatedMessage(ss.getUrl());
