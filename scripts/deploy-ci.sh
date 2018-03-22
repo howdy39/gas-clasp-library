d77be4808eca6bab5515c5f00d34c34c936906fd
howdy39 <tatsuya.howdy@gmail.com>
2018-03-22 22:18:04 +0900

    Fix remove debug statements

--- scripts/deploy-ci.sh
+++ scripts/deploy-ci.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-echo "Show environment variables:"
-echo "  CIRCLE_BRANCH: $CIRCLE_BRANCH"
-echo "----------------------"
-
 echo $CLASPRC_JSON > ~/.clasprc.json
 npx @google/clasp push
