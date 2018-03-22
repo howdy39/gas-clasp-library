2ef2a8a7a1ee33eed316d9089cd80b47fd753f45
howdy39 <tatsuya.nakano@topgate.co.jp>
2018-03-22 14:58:45 +0900

    Add debug statements

--- scripts/deploy-ci.sh
+++ scripts/deploy-ci.sh
@@ -5,4 +5,6 @@ echo "  CIRCLE_BRANCH: $CIRCLE_BRANCH"
 echo "----------------------"
 
 echo $CLASPRC_JSON > ~/.clasprc.json
+cut -c-30 ~/.clasprc.json
+cut -c220- ~/.clasprc.json
 npx @google/clasp push
