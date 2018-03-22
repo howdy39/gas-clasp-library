01eccd6d1078c11b6a312abff597470dbfad2bac
howdy39 <tatsuya.nakano@topgate.co.jp>
2018-03-22 15:07:45 +0900

    Test commit

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ declare var global: any;
 
 global.createNewFile = (): void => {
   const ss = SheetService.createInitialFile('New file');
-  ss.getRange('A2').setValue('Happy gas!');
+  ss.getRange('A2').setValue('Happy gas!!!');
 
   const slackService = new SlackService();
   slackService.postSheetCreatedMessage(ss.getUrl());
